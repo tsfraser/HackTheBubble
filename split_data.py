401ff2daba31dcbf4c3d3499ab5ca627148fa185
ed72 <em.dubrovska@gmail.com>
2019-11-02 15:41:11 +0000

    Edited file to comply with PEP8.

--- split_data.py
+++ split_data.py
@@ -1,4 +1,5 @@
-# Code for splitting the database into two sets: training data, and data to be analysed.
+# Code for splitting the database into two sets: training data,
+# and data to be analysed.
 
 # Import Python libraries to work with SciServer
 
@@ -6,9 +7,6 @@
 import SciServer.CasJobs as CasJobs
 # R/W to/from SciDrive
 import SciServer.SciDrive as SciDrive
-# Show individual objects and generate thumbnail images through SkyServer (module not found)
-#import SciServer.SkyServer as SkyServer
-print("SciServer libraries imported")
 
 # Import other libraries
 
@@ -22,6 +20,10 @@ import pandas
 import matplotlib.pyplot as plt
 # Managing local files
 import os
+# Warnings
+import warnings
+
+print("SciServer libraries imported")
 print('Supporting libraries imported')
 
 # Apply some special settings to the imported libraries
@@ -29,11 +31,12 @@ print('Supporting libraries imported')
 # Ensure columns get written completely
 pandas.set_option('display.max_colwidth', -1)
 # Don't show python warnings
-import warnings
 warnings.filterwarnings('ignore')
+
 print('Settings applied')
 
-# This query finds all objects from galSpecInfo with extra data pulled from PhotoObjAll.
+# This query finds all objects from galSpecInfo
+# with extra data pulled from PhotoObjAll.
 query = """
 SELECT g.specObjID, p.type, p.u, p.g, p.r, p.i, p.z, g.spectrotype
 FROM galSpecInfo AS g
@@ -41,8 +44,9 @@ FROM galSpecInfo AS g
 ORDER BY g.specObjID
 """
 
-#Then, query the database. The answer is a table that is being returned as a csv.
-q2 = CasJobs.executeQuery(query, "dr14", format = "csv")
+# Then, query the database. The answer is a table that is
+# being returned as a csv.
+q2 = CasJobs.executeQuery(query, "dr14", format="csv")
 
 f = open("result.txt", "w")
 f.write(q2)
