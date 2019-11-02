1a14c1050c5caab165b6fd80a9ab0cd1ecacb9c2
tsfraser <52163923+tsfraser@users.noreply.github.com>
2019-11-02 11:02:08 +0000

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,1 +1,46 @@
 # Code for splitting the database into two sets: training data, and data to be analysed.
+
+# Import Python libraries to work with SciServer
+import SciServer.CasJobs as CasJobs # query with CasJobs
+import SciServer.SciDrive as SciDrive   # read/write to/from SciDrive
+import SciServer.SkyServer as SkyServer   # show individual objects and generate thumbnail images through SkyServer
+print('SciServer libraries imported')
+
+# Import other libraries for use in this notebook.
+import numpy as np                  # standard Python lib for math ops
+from scipy.misc import imsave       # save images as files
+import pandas                       # data manipulation package
+import matplotlib.pyplot as plt     # another graphing package
+import os                           # manage local files in your Compute containers
+print('Supporting libraries imported')
+
+from astropy.io import ascii
+
+# Apply some special settings to the imported libraries
+# ensure columns get written completely in notebook
+pandas.set_option('display.max_colwidth', -1)
+# do *not* show python warnings 
+import warnings
+warnings.filterwarnings('ignore')
+print('Settings applied')
+
+
+# This query finds all galaxies in SDSS data release 14 (DR14) with a size (petror90_r) greater than 10 arcseconds, 
+# within a region of sky with 100 < RA < 250, a redshift between 0.02 and 0.5, and a g-band magnitude brighter than 17.
+# 
+# First, store the query in an object called "query"
+query="""
+SELECT p.objId,p.ra,p.dec,p.petror90_r, p.expAB_r,
+    p.dered_u as u, p.dered_g as g, p.dered_r as r, p.dered_i as i, 
+    s.z, s.plate, s.mjd, s.fiberid
+FROM galaxy AS p
+   JOIN SpecObj AS s ON s.bestobjid = p.objid
+WHERE p.petror90_r > 10
+  and p.ra between 100 and 250
+  and s.z between 0.02 and 0.1
+  and p.g < 17
+"""
+#Then, query the database. The answer is a table that is being returned to a dataframe that we've named all_gals.
+all_gals = CasJobs.executeQuery(query, "dr14")
+
+print("SQL query finished.")
